af393afc2f21ca11f35a84d006e7eae42833e519
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2019-12-02 16:26:46 +0100

    something

--- src/main/kotlin/be/moac/adventofcode/DayTwo.kt
+++ src/main/kotlin/be/moac/adventofcode/DayTwo.kt
@@ -4,13 +4,13 @@ import kotlin.system.measureTimeMillis
 
 interface DayTwo {
 
-    fun run(code: String): String
+    fun run(code: String): List<Int>
 
 }
 
 class IntCode: DayTwo {
 
-    override fun run(code: String): String {
+    override fun run(code: String): List<Int> {
         val memory = code.split(',').map { it.toInt() }.toMutableList()
 
         outer@ for (address in memory.indices step 4){
@@ -21,32 +21,30 @@ class IntCode: DayTwo {
             }
         }
 
-        return memory.joinToString(separator = ",")
+        return memory.toList()
     }
 }
 
 
 fun main() {
     val millis = measureTimeMillis {
-        println("result: ${partTwo()}")
+        val result = partTwo()
+        println("result: ${result.third}, noun: ${result.first}, verb: ${result.second}" )
     }
 
     println("finished in: $millis")
 }
 
-private fun partTwo(): Int {
+private fun partTwo(): Triple<Int, Int, Int> {
     (0..99).forEach { noun ->
         (0..99).forEach { verb ->
+            val input = "1,$noun,$verb,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,2,19,9,23,1,23,5,27,2,6,27,31,1,31,5,35,1,35,5,39,2,39,6,43,2,43,10,47,1,47,6,51,1,51,6,55,2,55,6,59,1,10,59,63,1,5,63,67,2,10,67,71,1,6,71,75,1,5,75,79,1,10,79,83,2,83,10,87,1,87,9,91,1,91,10,95,2,6,95,99,1,5,99,103,1,103,13,107,1,107,10,111,2,9,111,115,1,115,6,119,2,13,119,123,1,123,6,127,1,5,127,131,2,6,131,135,2,6,135,139,1,139,5,143,1,143,10,147,1,147,2,151,1,151,13,0,99,2,0,14,0"
 
-            val input =
-                "1,$noun,$verb,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,2,19,9,23,1,23,5,27,2,6,27,31,1,31,5,35,1,35,5,39,2,39,6,43,2,43,10,47,1,47,6,51,1,51,6,55,2,55,6,59,1,10,59,63,1,5,63,67,2,10,67,71,1,6,71,75,1,5,75,79,1,10,79,83,2,83,10,87,1,87,9,91,1,91,10,95,2,6,95,99,1,5,99,103,1,103,13,107,1,107,10,111,2,9,111,115,1,115,6,119,2,13,119,123,1,123,6,127,1,5,127,131,2,6,131,135,2,6,135,139,1,139,5,143,1,143,10,147,1,147,2,151,1,151,13,0,99,2,0,14,0"
-
-            val result = IntCode().run(input)
-            if (result.split(',')[0] == "19690720") {
-                return (100 * noun) + verb
+            if (IntCode().run(input)[0] == 19690720) {
+                return Triple(noun, verb, (100 * noun) + verb)
             }
         }
     }
 
-    return 0
+    return Triple(0,0,0)
 }
--- src/test/kotlin/be/moac/adventofcode/DayTwoTest.kt
+++ src/test/kotlin/be/moac/adventofcode/DayTwoTest.kt
@@ -16,7 +16,7 @@ internal class DayTwoTest {
         val input = "1,1,2,3"
 
         //When:
-        val result = dayTwo.run(input)
+        val result = dayTwo.run(input).joinToString(",")
 
         //Then:
         assertThat(result).isEqualTo("1,1,2,3")
@@ -30,7 +30,7 @@ internal class DayTwoTest {
         val input = "1,4,5,5,99,1"
 
         //When:
-        val result = dayTwo.run(input)
+        val result = dayTwo.run(input).joinToString(",")
 
         //Then:
         assertThat(result).isEqualTo("1,4,5,5,99,100")
@@ -43,7 +43,7 @@ internal class DayTwoTest {
         val input = "2,4,5,5,99,2"
 
         //When:
-        val result = dayTwo.run(input)
+        val result = dayTwo.run(input).joinToString(",")
 
         //Then:
         assertThat(result).isEqualTo("2,4,5,5,99,198")
@@ -60,7 +60,7 @@ internal class DayTwoTest {
     fun `that some more inputs works as expected`(input: String, expected: String) {
 
         //When:
-        val result = dayTwo.run(input)
+        val result = dayTwo.run(input).joinToString(",")
 
         //Then:
         assertThat(result).isEqualTo(expected)
@@ -77,6 +77,6 @@ internal class DayTwoTest {
         val result = dayTwo.run(input)
 
         //Then:
-        assertThat(result).isEqualTo("4576384,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,2,19,9,72,1,23,5,73,2,6,27,146,1,31,5,147,1,35,5,148,2,39,6,296,2,43,10,1184,1,47,6,1186,1,51,6,1188,2,55,6,2376,1,10,59,2380,1,5,63,2381,2,10,67,9524,1,6,71,9526,1,5,75,9527,1,10,79,9531,2,83,10,38124,1,87,9,38127,1,91,10,38131,2,6,95,76262,1,5,99,76263,1,103,13,76268,1,107,10,76272,2,9,111,228816,1,115,6,228818,2,13,119,1144090,1,123,6,1144092,1,5,127,1144093,2,6,131,2288186,2,6,135,4576372,1,139,5,4576373,1,143,10,4576377,1,147,2,4576379,1,151,13,0,99,2,0,14,0")
+        assertThat(result[0]).isEqualTo(4576384)
     }
 }
