5ea94028255c1f54e35cee7ab2c9b44f1ad33aba
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2019-12-09 21:23:59 +0100

    Day 8 part two

--- src/main/kotlin/be/moac/adventofcode/Day08.kt
+++ src/main/kotlin/be/moac/adventofcode/Day08.kt
@@ -2,18 +2,47 @@ package be.moac.adventofcode
 
 object DayEight {
 
-    fun checksum(image: Image): Int {
-
-        val layers = mutableListOf<String>()
-
-        for(i in image.value.indices step (image.width * image.height)) {
-            layers.add(image.value.slice(i until (i + (image.width * image.height))))
+    fun checksum(image: Image): Int =
+        with(image.layers.minBy { layer -> layer.count { it == '0' } } ?: "") {
+            count { it == '1' } * count { it == '2' }
         }
 
-        val selectedLayer = layers.minBy { layer -> layer.count { it == '0' } } ?: ""
-        return selectedLayer.count { it == '1' } * selectedLayer.count { it == '2' }
-    }
+    fun render(image: Image): String =
+        image.layers.fold(image.size.asString("2")) {result, layer ->
+            result.zip(layer).map { (left, right) ->
+                when(left) {
+                    '2' -> right
+                    else -> left
+                }
+            }.joinToString(separator = "")
+        }.also {
+            for(i in it.indices step (image.width)) {
+                println(it.slice(i until (i + (image.width))).map {
+                    when(it) {
+                        '0' -> '.'
+                        '1' -> '0'
+                        else -> ' '
+                    }
+                })
+            }
+        }
 
 }
 
-data class Image(val value: String, val width: Int, val height: Int)
+data class Image(val value: String, val width: Int, val height: Int) {
+
+    val size = (width * height)
+
+    val layers get() =  mutableListOf<String>().apply {
+        for(i in value.indices step (width * height)) {
+            add(value.slice(i until (i + (width * height))))
+        }
+    }.toList()
+
+
+
+}
+
+fun Int.asString(value: String): String =
+    (0 until this).joinToString(separator = "") { value }
+
