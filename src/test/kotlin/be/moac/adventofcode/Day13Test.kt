de6ff647678d0215a4987545342c69b0915fecea
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2019-12-13 16:16:43 +0100

    Day 13 - cleanup

--- src/test/kotlin/be/moac/adventofcode/Day13Test.kt
+++ src/test/kotlin/be/moac/adventofcode/Day13Test.kt
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test
 internal class Day13Test {
 
     @Test
-    fun `run the game`() {
-
-        //Given:
-
+    fun `run the game - part one`() {
         //When:
         val result = Arcade().partOne()
 
         //Then:
         assertThat(result).isEqualTo(236)
+
+    }
+    @Test
+    fun `run the game - part two`() {
+        //When:
+        val result = Arcade().partTwo()
+
+        //Then:
+        assertThat(result).isEqualTo(11040)
 
     }
 
